cc2f0250811a39d596c7f8560ab00eb67168ca85
sudhanshu-dhawan <134911547+sudhanshu-dhawan@users.noreply.github.com>
2023-07-10 13:58:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+alert("Welcome To DHAWAN HEALTHCARE");
+function onclick(){
+   
+}
+$(".btn").on("click" , function(){
+    prompt("enter your name  and contact number");
+    alert("Thanks for booking , we will contact you within an hour");
+});
+
+$(".app").on("click" , function(){
+    alert("you do not have any appointments at the moment");
+})
+$(".about").on("click" , function(){
+  
+})
